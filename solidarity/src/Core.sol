f46be10f2b29dbaff5ab46500c36d8ae781104c3
Jules Lordet <jules.lordet@epitech.eu>
2025-02-08 04:36:10 +0100

    [ADD] fund & refund & basic function

--- solidarity/src/Core.sol
+++ solidarity/src/Core.sol
@@ -6,26 +6,114 @@ contract Core {
     string public name;
     string public symbol;
     uint8 public decimals;
+    uint256 public fundsNeeded;
+    uint256 internal fundsRaised = 0;
     uint256 public totalSupply;
     uint256 public airDropAmount;
     uint256 public duration;
+    uint256 public endTime;
+    bool public projectInProd = false;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply, uint256 _airDropAmount, uint256 _duration) {
+    struct Contributor {
+        address contributor;
+        uint256 amount;
+    }
+    Contributor[] public contributors;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _fundNeeded,
+        uint256 _totalSupply,
+        uint256 _airDropAmount,
+        uint256 _duration
+    ) {
         owner = msg.sender;
         name = _name;
         symbol = _symbol;
+        fundsNeeded = _fundNeeded;
         decimals = _decimals;
         totalSupply = _totalSupply;
         airDropAmount = _airDropAmount;
         duration = _duration;
+        endTime = block.timestamp + _duration;
     }
+
+    event ProjectStarted(string message);
+    event projectRefunded(string message);    
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
 
+    modifier onlyAfterEndTime() {
+        require(block.timestamp >= endTime, "Function not available yet");
+        _;
+    }
+
+    modifier maxFundsNeededReached() {
+        require(fundsNeeded > fundsRaised, "Max funds needed reached");
+        _;
+    }
+
+    modifier projectInProduction() {
+        require(projectInProd == true, "Project is not in production");
+        _;
+    }
+
+    modifier projectNotInProduction() {
+        require(projectInProd == false, "Project is in production");
+        _;
+    }
+
     function setOwner(address _owner) public onlyOwner {
         owner = _owner;
+    }
+
+    function fundProject() public payable projectNotInProduction maxFundsNeededReached{
+        require(msg.value > 0, "Amount must be greater than 0");
+        contributors.push(Contributor(msg.sender, msg.value));
+        setFundsRaised(fundsRaised + msg.value);
+        emit projectRefunded("Project has been refunded");
+        if (fundsRaised >= fundsNeeded) {
+            startProjectProd();
+        }
+    }
+
+    function refundContributor() public onlyAfterEndTime {
+        for (uint256 i = 0; i < contributors.length; i++) {
+            if (contributors[i].amount > 0) {
+                payable(contributors[i].contributor).transfer(contributors[i].amount);
+                delete contributors[i];
+            }
+        }
+    }
+
+    function startProjectProd() internal {
+        projectInProd = true;
+        emit ProjectStarted("Project is now in production");
+    }
+
+    function setFundsRaised(uint256 _fundsRaised) internal {
+        fundsRaised = _fundsRaised;
+    }
+
+    function getFundsRaised() public view returns (uint256) {
+        return fundsRaised;
+    }
+
+    function getContributors() public view returns (Contributor[] memory) {
+        return contributors;
+    }
+
+    function getContributedValue(address _contributor) public view returns (uint256) {
+        for (uint256 i = 0; i < contributors.length; i++) {
+            if (contributors[i].contributor == _contributor) {
+                return contributors[i].amount;
+            }
+        }
+        return 0;
     }
 }
